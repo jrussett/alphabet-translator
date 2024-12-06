560d88d06835077a96cbf78223a7a4f6b9042ff3
Josh Russett <34522279+jrussett@users.noreply.github.com>
2024-12-05 17:15:37 -0800

    Add initial func to read config from file
    This function is really simple, it just reads the contents of the file
    and then passes the bytes off to the other function.
    
    In the future, it would probably make sense to stop creating the
    validConfigBytes before every test. I should probably put that in a
    BeforeSuite.

--- config/config.go
+++ config/config.go
@@ -1,10 +1,22 @@
 package config
 
-import "gopkg.in/yaml.v3"
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
 
 type LanguageConfig struct {
 	Name   string            `yaml:"name"`
 	Glyphs map[string]string `yaml:"glyphs"`
+}
+
+func NewLanguageConfigFromFile(configFilePath string) (*LanguageConfig, error) {
+	configBytes, err := os.ReadFile(configFilePath)
+	if err != nil {
+		return nil, err
+	}
+	return NewLanguageConfigFromBytes(configBytes)
 }
 
 func NewLanguageConfigFromBytes(bytes []byte) (*LanguageConfig, error) {
--- config/config_test.go
+++ config/config_test.go
@@ -1,6 +1,8 @@
 package config_test
 
 import (
+	"os"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
@@ -8,6 +10,19 @@ import (
 )
 
 var _ = Describe("Config", func() {
+	var validConfigBytes []byte
+
+	BeforeEach(func() { // TODO: consider doing this in a BeforeSuite
+		validConfigBytes = []byte(`
+---
+name: potato
+glyphs:
+  a: α
+  e: ε
+  i: ι
+`)
+	})
+
 	Describe("NewLanguageConfigFromBytes", func() {
 		var testConfig []byte
 
@@ -25,14 +40,7 @@ var _ = Describe("Config", func() {
 
 		Context("when the bytes are valid", func() {
 			BeforeEach(func() {
-				testConfig = []byte(`
----
-name: potato
-glyphs:
-  a: α
-  e: ε
-  i: ι
-`)
+				testConfig = validConfigBytes
 			})
 
 			It("returns a valid LanguageConfig", func() {
@@ -43,6 +51,48 @@ glyphs:
 				Expect(cfg.Glyphs).To(HaveKeyWithValue("a", "α"))
 				Expect(cfg.Glyphs).To(HaveKeyWithValue("e", "ε"))
 				Expect(cfg.Glyphs).To(HaveKeyWithValue("i", "ι"))
+			})
+		})
+	})
+
+	Describe("NewLanguageConfigFromFile", func() {
+		var filePath string
+
+		Context("when the file doesn't exist", func() {
+			BeforeEach(func() {
+				filePath = "/totally/real/file/path/config.yml"
+			})
+
+			It("errors", func() {
+				cfg, err := config.NewLanguageConfigFromFile(filePath)
+				Expect(err).To(HaveOccurred())
+				Expect(cfg).To(BeNil())
+			})
+
+			Context("when the file exists and is valid", func() {
+				BeforeEach(func() {
+					tempFile, err := os.CreateTemp("", "langconfig")
+					Expect(err).NotTo(HaveOccurred())
+
+					_, err = tempFile.Write(validConfigBytes)
+					Expect(err).NotTo(HaveOccurred())
+
+					filePath = tempFile.Name()
+
+					err = tempFile.Close()
+					Expect(err).NotTo(HaveOccurred())
+
+				})
+
+				It("returns a valid LanguageConfig", func() {
+					cfg, err := config.NewLanguageConfigFromFile(filePath)
+					Expect(err).NotTo(HaveOccurred())
+
+					Expect(cfg.Name).To(Equal("potato"))
+					Expect(cfg.Glyphs).To(HaveKeyWithValue("a", "α"))
+					Expect(cfg.Glyphs).To(HaveKeyWithValue("e", "ε"))
+					Expect(cfg.Glyphs).To(HaveKeyWithValue("i", "ι"))
+				})
 			})
 		})
 	})
